714ad732c054f7063a0cddea316b55783206adff
jyj <yjjiangphysics@gmail.com>
2020-08-26 21:08:37 -0500

    Updates

---
+++ precache-manifest.95de8d61c7286bf57f3740da9e553ce5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f9927bc146986d9cc0fac921ab53a0",
+    "url": "/reactmath/index.html"
+  },
+  {
+    "revision": "1f29138a416ec5803df0",
+    "url": "/reactmath/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99a770f2f5bdbdbc119c",
+    "url": "/reactmath/static/js/2.dcda58fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactmath/static/js/2.dcda58fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f29138a416ec5803df0",
+    "url": "/reactmath/static/js/main.976056b6.chunk.js"
+  },
+  {
+    "revision": "830057f1a23d3a788760",
+    "url": "/reactmath/static/js/runtime-main.9186dc3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactmath/static/media/logo.5d5d9eef.svg"
+  }
+]);
